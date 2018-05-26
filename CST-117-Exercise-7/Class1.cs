1bb59a0eb83cf5eb90b4b1e70ffc8579dc3020fa
Rihoj <rihoj1@gmail.com>
2018-05-26 17:08:04 -0400

    These are my stubs

--- CST-117-Exercise-7/Class1.cs
+++ CST-117-Exercise-7/Class1.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CST_117_Exercise_7
 {
-    public class Class1
+    public class ExerciseSeven
     {
+        public void showSum(int firstNum, int secondNum) { }
+
+        public double averageFiveDoubles(double firstDouble, double secondDouble, double thridDouble, double fourthDouble, double fifthDouble) { }
+
+        public int sumRandom() { }
+
+        public bool isDivisible(int firstNum, int secondNum, int thirdNum) { }
+
+        public void showLessCharacters(string firstString, string secondString) { }
+
+        public int[] generateRandomInts() { }
+
+        public bool areBoolsEqual(bool firstBool, bool secondBool) { }
+
+        public double product(int fistNum, double secondDouble) { }
+
+        public int averageTwoDimArray(int[][] integers) { }
     }
 }
